68892cf102fc8bf605fdc092f4a0fbb74dd45c1a
MysticMessages <114687002+MysticMessages@users.noreply.github.com>
2022-09-29 14:30:11 -0400

    Create tictactoe.cpp

---
+++ tictactoe.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "ttt_functions.hpp"
+
+int main () {
+  //setup
+  int p1 = 0;
+  int p2 = 0;
+  char s1 = '1';
+  char s2 = '2';
+  char s3 = '3';
+  char s4 = '4';
+  char s5 = '5';
+  char s6 = '6';
+  char s7 = '7';
+  char s8 = '8';
+  char s9 = '9';
+  std::cout << "-----------\n";
+  std::cout << "Tic Tac Toe\n";
+  std::cout << "-----------\n";
+  std::cout << "Player 1 = X\n";
+  std::cout << "Player 2 = O\n";
+  int playerturn1(char p1);
+  
+}
